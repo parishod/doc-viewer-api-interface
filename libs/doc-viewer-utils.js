528e61ab971be29f1d81ccf0b1fd01a88d16623f
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-01-27 16:59:54 -0600

    Added functions to get file extension and to load text file.

--- libs/doc-viewer-utils.js
+++ libs/doc-viewer-utils.js
@@ -4,6 +4,43 @@
  */
 "use strict";
 
+function getFileExtension(path) {
+    let fUrl = path;
+    fUrl = fUrl.toLowerCase();
+    // Returns file extension. Returns "" if no valid extension
+    // Ref: http://stackoverflow.com/a/1203361/3439460
+    return fUrl.substr((~-fUrl.lastIndexOf(".") >>> 0) + 2);
+}
+
+function loadTextFile(url) {
+    // Create new promise with the Promise() constructor;
+    // This has as its argument a function
+    // with two parameters, resolve and reject
+    return new Promise(function(resolve, reject) {
+        // Standard XHR to load json
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.responseType = 'text';
+        // When the request loads, check whether it was successful
+        request.onload = function() {
+            if (request.status === 200) {
+                // If successful, resolve the promise by passing back the request response
+                resolve(request.responseText);
+            } else {
+                // If it fails, reject the promise with a error message
+                reject(Error('Give file didn\'t load successfully; error code:' + request.statusText));
+            }
+        };
+        request.onerror = function() {
+            // Also deal with the case when the entire request fails to begin with
+            // This is probably a network error, so reject the promise with an appropriate message
+            reject(Error('There was a network error.'));
+        };
+        // Send the request
+        request.send();
+    });
+}
+
 function getVar(name)
 {
     var get_string = document.location.search;

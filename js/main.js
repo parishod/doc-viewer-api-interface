60f68cf226f1c067019d6a3f3f064c0f5801766b
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-01-26 20:42:13 -0600

    Added condition to return on null value in URL parameter.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 "use strict";
 
 function loadUrlInIframe(fileUrl, elementIdToAppend) {
+    if(fileUrl === "" || typeof fileUrl == "undefined" || fileUrl === null) {return;}
     var ifrm = document.createElement('iframe');
     ifrm.setAttribute('id', 'ifrm'); // assign an id
     ifrm.setAttribute('style', "border: 0; position:absolute; top:0; left:0; right:0; bottom:0; width:100%; height:100%;");

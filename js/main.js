8e96ea2375fc4aa7eebb69d2cf2a401d03024703
Uday Swarnapuri <swarnapuriuday@gmail.com>
2016-02-09 09:31:23 +0530

    Added the changes to read and write the cookie.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
  */
 "use strict";
 
-function loadUrlInIframe(fileUrl, elementIdToAppend) {
+function loadUrlInIframe(fileUrl, elementIdToAppend, extension) {
     if(fileUrl === "" || typeof fileUrl == "undefined" || fileUrl === null) {return;}
     var ifrm = document.createElement('iframe');
     ifrm.setAttribute('id', 'ifrm'); // assign an id
@@ -18,20 +18,75 @@ function loadUrlInIframe(fileUrl, elementIdToAppend) {
     el.parentNode.insertBefore(ifrm, el);
 
     // assign url
-    ifrm.setAttribute('src', 'https://docs.google.com/viewer?url='+fileUrl+'&embedded=true&chrome=true&dov=1');
+    ifrm.setAttribute('src', 'https://view.officeapps.live.com/op/view.aspx?src='+fileUrl);
 }
 
 let givenFileUrl = (typeof(getVar("url")) !== "undefined")? getVar("url"): "";
 
-// Loading the URL passed via API in Iframe
-loadUrlInIframe(givenFileUrl, 'document-viewing-frame');
-
-// Assigning Href to Download button in floating menu
-assignAttrToDocumentElementById("href", givenFileUrl, "floating-menu-main-download-id");
-
-loadFile("https://dl.dropboxusercontent.com/u/39352517/services.json", "json").then(function(response) {
+var jsonData;
+//Loading json file data
+loadFile("../config/user-preferences-default.json", "json").then(function(response) {
         // your code here
 		console.log("Response : " , response);
+		jsonData = response;
+		/*console.log("JsonData : ", jsonData.data.services[0].name);
+		console.log("JsonData : ", jsonData.data.services[0].file_extensions[0]);
+		console.log("JsonData : ", jsonData.data.services[0].file_open_API);*/
+		
+		
+		//Read cookie, if exists open the dowcument with given preference else chose default and write to cookie.
+		//Read the extension from url
+		//var startIndex = givenFileUrl.lastIndexOf(".");
+		//var endIndex = givenFileUrl.length;
+		var cookiePref;
+		if(givenFileUrl.lastIndexOf("&filetype") == -1){ // If the given url doesn't contain filetype extract extension
+			//var ext = givenFileUrl.substr(startIndex+1, endIndex);
+			var extFromUrl = getFileExtension(givenFileUrl);
+			var prefService;
+			var i;
+			cookiePref = readCookie("userPref");
+			console.log("Url Extension = " , extFromUrl);
+			if( cookiePref != null){
+				console.log("cooke Read: ", cookiePref);
+				//Replace %20 character with space.
+				//cookiePref = cookiePref.replace("%20", " ")	
+				//Read the preference for the given extension
+				var jsonFormatData = JSON.parse(cookiePref);
+				for(i=0; i<jsonFormatData.user_preferences.file_types.length; i++){
+					var tempExt = jsonFormatData.user_preferences.file_types[i].extension;
+					if(tempExt.localeCompare(extFromUrl) == 0){
+						prefService = jsonFormatData.user_preferences.file_types[i].preferred_service;
+						break;
+					}
+				}
+			}
+			else{
+				//createCookie(ext,"Google Docs",10, "/");	
+				//Just to verify if cookie is created successfully or not
+				//console.log(readCookie(ext));
+				createCookie("userPref", JSON.stringify(jsonData), 10, "/");	
+				console.log("Created cookie : ",JSON.parse(readCookie("userPref")));
+				
+				//Read the default service
+				for(i=0; i<jsonData.user_preferences.file_types.length; i++){
+					var tempExt = jsonData.user_preferences.file_types[i].extension;
+					if(tempExt.localeCompare(extFromUrl) == 0){
+						prefService = jsonData.user_preferences.file_types[i].preferred_service;
+						break;
+					}
+				}
+			}
+			// Loading the URL passed via API in Iframe
+			loadUrlInIframe(givenFileUrl, 'document-viewing-frame', extFromUrl);
+		}else{
+			console.log("File type found ");
+			// Loading the URL passed via API in Iframe
+			loadUrlInIframe(givenFileUrl, 'document-viewing-frame');
+		}
 }, function(Error) {
     console.log(Error);
 });
+
+
+// Assigning Href to Download button in floating menu
+assignAttrToDocumentElementById("href", givenFileUrl, "floating-menu-main-download-id");
--- libs/doc-viewer-utils.js
+++ libs/doc-viewer-utils.js
@@ -118,3 +118,55 @@ function assignAttrToDocumentElementById(attribute, attributeValue, elementId) {
         }
     }
 }
+
+//======= Functions to Read and Write Cookies ======
+/**
+ * Creates Cookie to save the user preferences for api to be used for a given file type.
+ * @param {String} name 
+ * @param {String} value
+ * @return {Number} days
+ */
+function createCookie(name, value, expires, path, domain) {
+	//console.log("createCookie Called");
+	var cookie = name + "=" + escape(value) + ";";
+
+	if (expires) {
+	// If it's a date
+	if(expires instanceof Date) {
+	  // If it isn't a valid date
+	  if (isNaN(expires.getTime()))
+	   expires = new Date();
+	}
+	else
+		expires = new Date(new Date().getTime() + parseInt(expires) * 1000 * 60 * 60 * 24);
+
+		cookie += "expires=" + expires.toGMTString() + ";";
+	}
+
+	if (path)
+		cookie += "path=" + path + ";";
+	if (domain)
+		cookie += "domain=" + domain + ";";
+
+	document.cookie = cookie;
+}
+
+/**
+ * Reads Cookie to know the user preferences for api to be used for a given file type.
+ * @param {String} name 
+ */
+function readCookie(name) {
+	//console.log("ReadCookie Called");
+	var regexp = new RegExp("(?:^" + name + "|;\s*"+ name + ")=(.*?)(?:;|$)", "g");
+	var result = regexp.exec(document.cookie);
+	return (result === null) ? null : result[1];
+}
+
+/**
+ * Deletes the given Cookie.
+ * @param {String} name 
+ */
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+//======= Functions to Read and Write Cookies End ====== 

100baf52fc93a94225b1c326b56dc3be0d3c07bb
Uday Swarnapuri <swarnapuriuday@gmail.com>
2016-02-23 09:20:26 +0530

    close #14 , settings disabled for a single service

--- js/settings.js
+++ js/settings.js
@@ -11,7 +11,30 @@ function getFileTypesSettigsContent(fileType, supportedServicesArr){
             return prev+`<option>${curr}</option>`;
         }, "");
     let formId = `service-option-${fileType.toLowerCase()}`;
-
+    //Reference : http://stackoverflow.com/questions/881085/count-the-number-of-occurences-of-a-character-in-a-string-in-javascript
+    //console.log(formOptions.split("option").length - 1);
+    let numAvailableServices = (formOptions.split("option").length - 1)/2; //Searching option string 2 occurences implies one service
+    
+    return (numAvailableServices === 1)
+        ?`
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="form-group">
+                <label for="${formId}">${fileType.toUpperCase()}</label>
+                <select class="form-control" id="${formId}" disabled>
+					${formOptions}
+                </select>
+            </div>
+        </div>`
+        :`
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="form-group">
+                <label for="${formId}">${fileType.toUpperCase()}</label>
+                <select class="form-control" id="${formId}">
+					${formOptions}
+                </select>
+            </div>
+        </div>`
+    /*
     return `
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
             <div class="form-group">
@@ -21,5 +44,6 @@ function getFileTypesSettigsContent(fileType, supportedServicesArr){
                 </select>
             </div>
         </div>`;
+    */
 }
 

d3e1446406688539ddc1004bea3ac2bf83e3cc2b
Deekshith Allamaneni <dkh.hyd@gmail.com>
2016-02-22 23:52:04 -0600

    refactor #14: checking num of services by using supportedServiceArr length, minimized code redundancy.

--- js/settings.js
+++ js/settings.js
@@ -10,29 +10,17 @@ function getFileTypesSettigsContent(fileType, supportedServicesArr){
         .reduce((prev, curr) => {
             return prev+`<option>${curr}</option>`;
         }, "");
-    let formId = `service-option-${fileType.toLowerCase()}`;
-    //Reference : http://stackoverflow.com/questions/881085/count-the-number-of-occurences-of-a-character-in-a-string-in-javascript
-    //console.log(formOptions.split("option").length - 1);
-    let numAvailableServices = (formOptions.split("option").length - 1)/2; //Searching option string 2 occurences implies one service
-    
-    return (numAvailableServices === 1)
-        ?`
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+    const formId = `service-option-${fileType.toLowerCase()}`;
+    let formStatus = (supportedServicesArr.length === 1)
+        ? `disabled`
+        : ``;
+    return `<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
             <div class="form-group">
                 <label for="${formId}">${fileType.toUpperCase()}</label>
-                <select class="form-control" id="${formId}" disabled>
+                <select class="form-control" id="${formId}" ${formStatus}>
 					${formOptions}
                 </select>
             </div>
-        </div>`
-        :`
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-            <div class="form-group">
-                <label for="${formId}">${fileType.toUpperCase()}</label>
-                <select class="form-control" id="${formId}">
-					${formOptions}
-                </select>
-            </div>
-        </div>`
+        </div>`;
 }
 

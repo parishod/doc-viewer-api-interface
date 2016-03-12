f4ecd935ce157638b01897c36cd00508f7002daf
Uday Swarnapuri <swarnapuriuday@gmail.com>
2016-03-12 09:01:18 +0530

    feat: saving user-pref partially implemented for only doc extension as of now

--- js/settings.js
+++ js/settings.js
@@ -29,4 +29,27 @@ document.getElementById("settings-services-tab-row").onchange=function(){ //run 
     let selectmenu=document.getElementById("service-option-doc");
     let chosenoption=selectmenu.options[selectmenu.selectedIndex].value 
     console.log("Selected item : ", chosenoption);
+    
+    try {
+        let fileExtensionOfUrl = `doc`;
+        let jsonFormatData = JSON.parse(decodeURIComponent(localStorage.getItem('viewer-user-pref')));
+        let thisUserConfiguration = new AnyFileViewerUserConfig(jsonFormatData);
+        //console.log("localStorage.getItem('viewer-user-pref'):", localStorage.getItem('viewer-user-pref')); //DEBUG
+        let indexPreferredService = jsonFormatData.user_preferences.file_types
+            .findIndex((thisFileTypeObj) => thisFileTypeObj.extension === fileExtensionOfUrl);
+        
+        console.log("getElementById indexPreferredService :", indexPreferredService); //DEBUG
+        if(indexPreferredService !== -1) {
+            let prefService = jsonFormatData.user_preferences.file_types[indexPreferredService].preferred_service = chosenoption;
+            //prefService = jsonFormatData.user_preferences.file_types[indexPreferredService].preferred_service
+            localStorage.setItem('viewer-user-pref', JSON.stringify(jsonFormatData));
+            console.log("prefService :", prefService); //DEBUG
+        }else {
+            // This can happen when bad URL is used. Like http://domain.com/viewer?url=
+            throw `getElementById Could not find preferred service id for URL ${fileExtensionOfUrl}`;
+        }
+
+    } catch (err) {
+        console.error("getElementById Error: ", err);
+    }
 }
